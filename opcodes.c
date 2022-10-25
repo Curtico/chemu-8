ff972062b9ddbf3c49a2ed584655f69a0e4f8321
Curtice Gough <31411535+Curtico@users.noreply.github.com>
2022-10-25 13:34:22 -0400

    Add functionality for remaining opcodes to disassembler

--- opcodes.c
+++ opcodes.c
@@ -11,36 +11,67 @@ void print_instruction(uint16_t opcode) {
     switch (first_nibble) {
         case 0x00: {
                        if (bytes[0] >> 4 != 0xe) {
-                           printf("CALL 0x%x%x\n", bytes[1] - (first_nibble * 16), bytes[0]);
+                           printf("CALL     0x%x%x\n", bytes[1] - (first_nibble * 16), bytes[0]);
                        }
                        else {
                            switch (bytes[0]) {
                                case 0xe0: printf("DISP_CLR\n"); break;
-                               case 0xee: printf("RETURN\n"); break;
+                               case 0xee: printf("RET\n"); break;
                            }
                        }
                        break;
                    }
-        case 0x01: printf("GOTO 0x%01x%02x\n", bytes[1] - (first_nibble * 16), bytes[0]); break;
-        case 0x02: printf("CALL 0x%01x%02x\n", bytes[1] - (first_nibble * 16), bytes[0]); break;
-        case 0x03: printf("SKIP_IF_EQUAL V%d, 0x%02x\n", bytes[1] - (first_nibble * 16), bytes[0]); break;
-        case 0x04: printf("SKIP_IF_NOT_EQUAL V%d, 0x%02x\n", bytes[1] - (first_nibble * 16), bytes[0]); break;
-        case 0x05: printf("SKIP_IF_EQUAL V%d, V%d\n", bytes[1] - (first_nibble * 16), bytes[0] >> 4); break;
-        case 0x06: printf("SET_REGISTER V%d, 0x%02x\n", bytes[1] - (first_nibble * 16), bytes[0]); break;
-        case 0x07: printf("ADD_REGISTER V%d, 0x%02x\n", bytes[1] - (first_nibble * 16), bytes[0]); break;
-        case 0x08: printf("Undetermined opcode\n"); break; // TODO
+        case 0x01: printf("GOTO     0x%01x%02x\n", bytes[1] - (first_nibble * 16), bytes[0]); break;
+        case 0x02: printf("CALL     0x%01x%02x\n", bytes[1] - (first_nibble * 16), bytes[0]); break;
+        case 0x03: printf("SKE      V%d, 0x%02x\n", bytes[1] - (first_nibble * 16), bytes[0]); break;
+        case 0x04: printf("SKNE     V%d, 0x%02x\n", bytes[1] - (first_nibble * 16), bytes[0]); break;
+        case 0x05: printf("SKE      V%d, V%d\n", bytes[1] - (first_nibble * 16), bytes[0] >> 4); break;
+        case 0x06: printf("SET      V%d, 0x%02x\n", bytes[1] - (first_nibble * 16), bytes[0]); break;
+        case 0x07: printf("ADD      V%d, 0x%02x\n", bytes[1] - (first_nibble * 16), bytes[0]); break;
+        case 0x08: {
+                       uint8_t vx = bytes[1] - (first_nibble * 16);
+                       uint8_t vy = bytes[0] >> 4;
+                       uint8_t operand = bytes[0] - (vy * 16);
+
+                       switch (operand) {
+                           case 0x0: printf("SET      V%d, V%d\n", vx, vy); break;
+                           case 0x1: printf("OR       V%d, V%d\n", vx, vy); break;
+                           case 0x2: printf("AND      V%d, V%d\n", vx, vy); break;
+                           case 0x3: printf("XOR      V%d, V%d\n", vx, vy); break;
+                           case 0x4: printf("ADD      V%d, V%d\n", vx, vy); break;
+                           case 0x5: printf("SUB      V%d, V%d\n", vx, vy); break;
+                           case 0x6: printf("SHIFTR   V%d\n", vx); break;
+                           case 0x7: printf("SUBX     V%d, V%d\n", vx, vy); break;
+                           case 0xe: printf("SHIFTL   V%d\n", vx); break;
+                       }
+                   }
+                   break;
         case 0x09: printf("SKIP_IF_NOT_EQUAL V%d, V%d\n", bytes[1] - (first_nibble * 16), bytes[0] >> 4); break;
-        case 0x0a: printf("SET_I 0x%01x%02x\n", bytes[1] - (first_nibble * 16), bytes[0]); break;
-        case 0x0b: printf("GOTO V0 + 0x%01x%02x\n", bytes[1] - (first_nibble * 16), bytes[0]); break;
-        case 0x0c: printf("SET_RAND V%d, 0x%02x\n", bytes[1] - (first_nibble * 16), bytes[0]); break;
-        case 0x0d: printf("DRAW V%d, V%d, %d\n", bytes[1] - (first_nibble * 16), bytes[0] >> 4, bytes[0] - ((bytes[0] >> 4) * 16)); break;
+        case 0x0a: printf("SET_I    0x%01x%02x\n", bytes[1] - (first_nibble * 16), bytes[0]); break;
+        case 0x0b: printf("GOTO     V0 + 0x%01x%02x\n", bytes[1] - (first_nibble * 16), bytes[0]); break;
+        case 0x0c: printf("RAND     V%d, 0x%02x\n", bytes[1] - (first_nibble * 16), bytes[0]); break;
+        case 0x0d: printf("DRAW     V%d, V%d, %d\n", bytes[1] - (first_nibble * 16), bytes[0] >> 4, bytes[0] - ((bytes[0] >> 4) * 16)); break;
         case 0x0e: {
                        switch (bytes[0]) {
-                           case 0x9e: printf("SKIP_IF_KEY_PRESSED V%d\n", bytes[1] - (first_nibble * 16)); break;
-                           case 0xa1: printf("SKIP_IF_KEY_NOT_PRESSED V%d\n", bytes[1] - (first_nibble * 16)); break;
+                           case 0x9e: printf("SKP      V%d\n", bytes[1] - (first_nibble * 16)); break;
+                           case 0xa1: printf("SKNP     V%d\n", bytes[1] - (first_nibble * 16)); break;
                        }
                        break;
                    }
-        case 0x0f: printf("Undetermined opcode\n"); break;
+        case 0x0f: {
+                       uint8_t vx = bytes[1] - (first_nibble * 16);
+                       switch (bytes[0]) {
+                           case 0x07: printf("SET      V%d, DT\n", vx); break;
+                           case 0x0a: printf("SET      V%d\n, KP", vx); break;
+                           case 0x15: printf("SET      DT, V%d\n", vx); break;
+                           case 0x18: printf("SET      ST, V%d\n", vx); break;
+                           case 0x1e: printf("ADD      I,  V%d\n", vx); break;
+                           case 0x29: printf("SPRITE   V%d\n", vx); break;
+                           case 0x33: printf("BCD      V%d\n", vx); break;
+                           case 0x55: printf("STU      V%d\n", vx); break;
+                           case 0x65: printf("LDU      V%d\n", vx); break;
+                       }
+                   }
+                   break;
     }
 }
